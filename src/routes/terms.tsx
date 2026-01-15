3fca48de1cd871d223e67f6f3919b482f870a1a4
Darryl <hjdjoo@gmail.com>
2026-01-15 17:47:34 -0500

    more changes to tos

--- src/routes/terms.tsx
+++ src/routes/terms.tsx
@@ -42,6 +42,21 @@ function TermsPage() {
             {/* Your content here */}
             This is meant to be a free tool for study as well as a paid tool for fitness enthusiasts to learn about the human body and find new exercises.
           </p>
+          <p>
+            You can do things like suggest exercises for a muscle and vote on exercises for visibility, but please don't abuse it.
+          </p>
+          <p>
+            We only really use your data to run the app and make sure the app is working properly. We won't ever sell your data or do anything shady like that.
+          </p>
+          <p>
+            When you subscribe, you get access to our exercise library and more clinical details. It's $1.99/mo. You can cancel your subscription at any time, and you'll keep access till the end of your subscription cycle.
+          </p>
+          <p>
+            This is meant for a slightly more adult audience, but we think it'll be useful for kids and students studying biology, too.
+          </p>
+          <p>
+            If you try to do anything weird while using the app, like trying to access other people's data or abusing it, we reserve the right to cancel your Premium services, or do things like block your IP address from accessing the app. Please don't. We are actually just one guy in three trenchcoats, and we just want to be helpful.
+          </p>
         </section>
       </div>
     </PageLayout>
